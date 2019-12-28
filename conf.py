640924664a1d7e31fc7d891f2d34a0a427600342
zrvgod <57533196+zrvgod@users.noreply.github.com>
2019-12-28 18:54:41 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,14 +14,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "ZR's Blog"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
+author = "ZR"
+email = "zrvgod@126.com"
+author_homepage = "https://idkzr.com"
 description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+key_words = ['Maverick', 'ZR', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
     {
